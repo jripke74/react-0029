9e91f425688f7b83147da94156dffe967fc56270
Jeff Ripke <jripke74@gmail.com>
2023-01-23 16:20:43 -0800

    add onLogout and onLogin

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from './store/auth-context';
 
 const app = document.getElementById('root');
 const root = createRoot(app);
-root.render(<App />);
+root.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+);
